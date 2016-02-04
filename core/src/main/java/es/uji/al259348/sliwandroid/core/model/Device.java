4d945b57a433372f7e4d39b09e112e5649145fe8
Adrian Puertas Cabedo <al259348@uji.es>
2016-02-04 17:08:27 +0100

    Deleted a pair of unused classes, added a bit of javadoc

--- core/src/main/java/es/uji/al259348/sliwandroid/core/model/Device.java
+++
@@ -1,8 +0,0 @@
-package es.uji.al259348.sliwandroid.core.model;
-
-public class Device {
-
-    private String id;
-    private User linkedUser;
-
-}
--- core/src/main/java/es/uji/al259348/sliwandroid/core/services/UserService.java
+++ core/src/main/java/es/uji/al259348/sliwandroid/core/services/UserService.java
@@ -6,7 +6,19 @@ import rx.Observable;
 
 public interface UserService {
 
+    /**
+     * Get the current linked user.
+     *
+     * @return Returns the current linked user or null if there isn't.
+     */
     User getCurrentLinkedUser();
+
+    /**
+     * Set the current linked user. It will be persisted.
+     *
+     * @param user The user to be linked or null to unlink the current user.
+     * @return Returns true if the new value were successfully written.
+     */
     boolean setCurrentLinkedUser(User user);
 
     Observable<User> getUserLinkedTo(String deviceId);
