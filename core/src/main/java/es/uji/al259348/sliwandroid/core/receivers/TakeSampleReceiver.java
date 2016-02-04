b20b7055ee3aadc8542e6a55a1ce5a24b111dd4a
Adrian Puertas Cabedo <al259348@uji.es>
2016-02-04 15:07:51 +0100

    TakeSampleReceiver, reacts when the alarm goes off, then takes a sample and send it to the broker

---
+++ core/src/main/java/es/uji/al259348/sliwandroid/core/receivers/TakeSampleReceiver.java
@@ -0,0 +1,73 @@
+package es.uji.al259348.sliwandroid.core.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+
+import java.util.Date;
+
+import es.uji.al259348.sliwandroid.core.R;
+import es.uji.al259348.sliwandroid.core.services.MQService;
+import es.uji.al259348.sliwandroid.core.services.MQServiceImpl;
+import es.uji.al259348.sliwandroid.core.services.WifiService;
+import es.uji.al259348.sliwandroid.core.services.WifiServiceImpl;
+
+public class TakeSampleReceiver extends BroadcastReceiver {
+
+    public TakeSampleReceiver() {
+
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("TakeSampleReceiver", "onReceive: " + (new Date()).toString());
+
+        String brokerHost = context.getResources().getString(R.string.mqtt_broker_host);
+        String brokerUser = context.getResources().getString(R.string.mqtt_broker_user);
+        String brokerPass = context.getResources().getString(R.string.mqtt_broker_pass);
+        String clientId = context.getResources().getString(R.string.mqtt_client_id);
+        Log.d("TakeSampleReceiver", "brokerHost: " + brokerHost);
+        Log.d("TakeSampleReceiver", "brokerUser: " + brokerUser);
+        Log.d("TakeSampleReceiver", "brokerPass: " + brokerPass);
+        Log.d("TakeSampleReceiver", "clientId: " + clientId);
+
+        MqttConnectOptions connectOptions = new MqttConnectOptions();
+        connectOptions.setCleanSession(true);
+        connectOptions.setUserName(brokerUser);
+        connectOptions.setPassword(brokerPass.toCharArray());
+
+        MqttAndroidClient mqttClient = new MqttAndroidClient(context.getApplicationContext(), brokerHost, clientId);
+
+        MQService mqService = new MQServiceImpl(mqttClient, connectOptions);
+        WifiService wifiService = new WifiServiceImpl(context.getApplicationContext());
+
+        wifiService.performScan()
+                .doOnError(Throwable::printStackTrace)
+                .doOnNext(sample -> {
+                    Log.d("TakeSampleReceiver", "Sample: " + sample);
+                    try {
+                        ObjectMapper objectMapper = new ObjectMapper();
+
+                        String topic = "user/1/sample";
+                        String msg = objectMapper.writeValueAsString(sample);
+
+                        mqService.publish(topic, msg)
+                                .doOnError(Throwable::printStackTrace)
+                                .doOnCompleted(() -> Log.d("TakeSampleReceiver", "Sample published!"))
+                                .subscribe();
+                    } catch (JsonProcessingException e) {
+                        e.printStackTrace();
+                    }
+
+                })
+                .subscribe();
+    }
+
+}
