a20b09459a462270888b7815f3e0f4136e06f83c
Adrian Puertas Cabedo <al259348@uji.es>
2016-06-01 23:46:27 +0200

    Sample service methods: countLocalSamples, getLocalSamples, remove, publish...

--- core/src/main/java/es/uji/al259348/sliwandroid/core/services/SampleService.java
+++ core/src/main/java/es/uji/al259348/sliwandroid/core/services/SampleService.java
@@ -1,5 +1,7 @@
 package es.uji.al259348.sliwandroid.core.services;
 
+import java.util.List;
+
 import es.uji.al259348.sliwandroid.core.model.Sample;
 import rx.Observable;
 
@@ -8,5 +10,10 @@ public interface SampleService extends Service {
     Observable<Sample> take();
 
     Sample save(Sample sample);
+    void remove(Sample sample);
 
+    long countLocalSamples();
+    List<Sample> getLocalSamples();
+
+    Observable<String> publish(Sample sample);
 }
